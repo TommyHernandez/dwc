8a7f99db8c88c4df7bd7907c2e61264001005f48
Tomás <PixelariumStudioCenter>
2014-11-02 20:38:46 +0100

    Subida ejercicioHF. Falta guardar el color del posit y pulir el que no meta ids duplicadas

--- ejerciciosHF/js/notas.js
+++ ejerciciosHF/js/notas.js
@@ -1,8 +1,9 @@
-function addStickyToDOM(value) {
+function addStickyToDOM(value, key) {
     var notas = document.getElementById("notas");
     var nota = document.createElement("li");
     var span = document.createElement("span");
     span.setAttribute("class", "sticky");
+    span.setAttribute("id", key);
     span.textContent = value;
     nota.appendChild(span);
     notas.appendChild(nota);
@@ -12,7 +13,20 @@ function createSticky() {
     var value = document.getElementById("note_text").value;
     var key = "nota_" + localStorage.length;
     localStorage.setItem(key, value);
-    addStickyToDOM(value);
+    addStickyToDOM(value, key);
+}
+
+function ejecutar(evt) {
+    if (confirm("Se eliminará esta nota " + this.id)) {
+        if (confirm("Está de acuerdo")) {
+            localStorage.removeItem(this.id);
+            document.getElementById(this.id).parentNode.innerHTML = "";
+
+        } else {}
+        return true;
+    } else {
+        evt.preventDefault();
+    }
 }
 
 function init() {
@@ -20,11 +34,15 @@ function init() {
         var key = localStorage.key(i);
         if (key.substring(0, 4) == "nota") {
             var value = localStorage.getItem(key);
-            addStickyToDOM(value);
-
+            addStickyToDOM(value, key);
         }
     }
     var button = document.getElementById("add_button");
+    var notas = document.getElementsByTagName("span");
+    for (var a = 0; a < notas.length; a++) {
+        notas[a].addEventListener('click', ejecutar, false);
+    }
     button.onclick = createSticky;
+
 }
 window.onload = init;
--- tema3/ejercicio2/js/main.js
+++ tema3/ejercicio2/js/main.js
@@ -1,10 +1,31 @@
-function main(){
-   var t1 = document.getElementById('titulo1');
-    t1.onclick = function (){
-        var invis = document.getElementById('Hardware');
-        invis.classList.toggle('invisible');
-    }
-    
+function main() {
+    var imagenh = [],
+        imagens = [];
+    imagenh[0] = "imagenes/motog.jpg";
+    imagenh[1] = "imagenes/nexus";
+    imagenh[2] = "imagenes/motog.jpg";
+    //Ahora introducimos la de software
+    imagens[0] = "imagenes/gimp.png";
+    var t1 = document.getElementById('titulo1'),
+        t2 = document.getElementById('titulo2');
+    var lista = document.getElementsByTagName("li");
+   
+
+
+    t1.addEventListener("click", function () {
+        var invis = document.getElementById('lista1');
+        invis.classList.toggle('oculto');
+
+
+    });
+    t2.addEventListener("click", function () {
+        var invis = document.getElementById('lista2');
+        invis.classList.toggle('oculto');
+
+    });
+
 }
+
+
 
 window.onload = main;
